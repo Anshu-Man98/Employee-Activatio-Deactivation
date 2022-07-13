2c5757110ec719e08c075a49fa9613d05ba46b11
Amnshuman <amnshuman.sunil.ext@wsa.com>
2022-07-13 17:01:35 +0530

    Upgraded from .net 5.0 to 3.1

--- EmployeeDeactivation/BusinessLayer/PdfDataOperation.cs
+++ EmployeeDeactivation/BusinessLayer/PdfDataOperation.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace EmployeeDeactivation.BusinessLayer
@@ -23,37 +24,74 @@ namespace EmployeeDeactivation.BusinessLayer
 
         public byte[] FillDeactivationPdfForm(string gId)
         {
-            var employeeData = _employeeDataOperation.RetrieveDeactivatedEmployeeDataBasedOnGid(gId);
-            FileStream docStream = new FileStream("Deactivation.pdf", FileMode.Open, FileAccess.Read);
-            PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
-            PdfLoadedForm form = loadedDocument.Form;
-            string EmailandGId =  employeeData.GId +", "+ employeeData.EmailID;
-            (form.Fields[11] as PdfLoadedTextBoxField).Text = employeeData.FirstName ?? "";
-            (form.Fields[11] as PdfLoadedTextBoxField).ReadOnly = true;
-            (form.Fields[10] as PdfLoadedTextBoxField).Text = employeeData.LastName ?? "";
-            (form.Fields[10] as PdfLoadedTextBoxField).ReadOnly = true;
-            (form.Fields[12] as PdfLoadedTextBoxField).Text = EmailandGId;
-            (form.Fields[12] as PdfLoadedTextBoxField).ReadOnly = true;
-            var LastWorkingDate = employeeData.LastWorkingDate.Day + " / " + employeeData.LastWorkingDate.Month + " / " +employeeData.LastWorkingDate.Year;
-            (form.Fields[4] as PdfLoadedTextBoxField).Text = LastWorkingDate ?? "";
-            (form.Fields[4] as PdfLoadedTextBoxField).ReadOnly = true;
-            (form.Fields[16] as PdfLoadedTextBoxField).Text = employeeData.SponsorLastName ?? "";
-            (form.Fields[16] as PdfLoadedTextBoxField).ReadOnly = false;
-            (form.Fields[17] as PdfLoadedTextBoxField).Text = employeeData.SponsorFirstName ?? "";
-            (form.Fields[17] as PdfLoadedTextBoxField).ReadOnly = false;
-            (form.Fields[18] as PdfLoadedTextBoxField).Text = employeeData.SponsorGId ?? "";
-            (form.Fields[18] as PdfLoadedTextBoxField).ReadOnly = false;
-            (form.Fields[19] as PdfLoadedTextBoxField).Text = employeeData.SponsorDepartment ?? "";
-            (form.Fields[19] as PdfLoadedTextBoxField).ReadOnly = false;
-            MemoryStream stream = new MemoryStream();
-            loadedDocument.Save(stream);
-            stream.Position = 0;
-            loadedDocument.Close(true);
-            return stream.ToArray();
+            try
+            {
+                var employeeData = _employeeDataOperation.RetrieveDeactivatedEmployeeDataBasedOnGid(gId);
+                FileStream docStream = new FileStream("Deactivation.pdf", FileMode.Open, FileAccess.Read);
+                PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
+                PdfLoadedForm form = loadedDocument.Form;
+                string EmailandGId = employeeData.GId + ", " + employeeData.EmailID;
+                (form.Fields[11] as PdfLoadedTextBoxField).Text = employeeData.FirstName ?? "";
+                (form.Fields[11] as PdfLoadedTextBoxField).ReadOnly = true;
+                (form.Fields[10] as PdfLoadedTextBoxField).Text = employeeData.LastName ?? "";
+                (form.Fields[10] as PdfLoadedTextBoxField).ReadOnly = true;
+                (form.Fields[12] as PdfLoadedTextBoxField).Text = EmailandGId;
+                (form.Fields[12] as PdfLoadedTextBoxField).ReadOnly = true;
+                var LastWorkingDate = employeeData.LastWorkingDate.Day + "/" + employeeData.LastWorkingDate.Month + "/" + employeeData.LastWorkingDate.Year;
+                (form.Fields[4] as PdfLoadedTextBoxField).Text = LastWorkingDate ?? "";
+                (form.Fields[4] as PdfLoadedTextBoxField).ReadOnly = false;
+                (form.Fields[16] as PdfLoadedTextBoxField).Text = employeeData.SponsorLastName ?? "";
+                (form.Fields[16] as PdfLoadedTextBoxField).ReadOnly = false;
+                (form.Fields[17] as PdfLoadedTextBoxField).Text = employeeData.SponsorFirstName ?? "";
+                (form.Fields[17] as PdfLoadedTextBoxField).ReadOnly = false;
+                (form.Fields[18] as PdfLoadedTextBoxField).Text = employeeData.SponsorGId ?? "";
+                (form.Fields[18] as PdfLoadedTextBoxField).ReadOnly = false;
+                (form.Fields[19] as PdfLoadedTextBoxField).Text = employeeData.SponsorDepartment ?? "";
+                (form.Fields[19] as PdfLoadedTextBoxField).ReadOnly = false;
+                MemoryStream stream = new MemoryStream();
+                loadedDocument.Save(stream);
+                stream.Position = 0;
+                loadedDocument.Close(true);
+                return stream.ToArray();
+            }
+                        
+            catch (Exception e)
+            {
+
+
+                string fileName = @"C:\Temp\FillPdfDeactivationError.txt";
+
+
+                if (File.Exists(fileName))
+
+                {
+
+                    File.Delete(fileName);
+
+                }
+
+                // Create a new file     
+
+                using (FileStream fs = File.Create(fileName))
+
+                {
+                    // Add some text to file    
+
+                    Byte[] title = new UTF8Encoding(true).GetBytes("New Text File");
+
+    fs.Write(title, 0, title.Length);
+                    byte[] text = new UTF8Encoding(true).GetBytes("ERROR------------------> " + e.StackTrace);
+
+    fs.Write(text);
+                    return null;
+
+                }
+            }
         }
 
         public byte[] HelpPdfDeactivation()
         {
+            try { 
             FileStream docStream = new FileStream("EMPLOYEE ACTIVATION-DEACTIVATION WORKFLOW.pdf", FileMode.Open, FileAccess.Read);
             PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
             MemoryStream stream = new MemoryStream();
@@ -61,6 +99,39 @@ namespace EmployeeDeactivation.BusinessLayer
             stream.Position = 0;
             loadedDocument.Close(true);
             return stream.ToArray();
+            }
+            catch (Exception e)
+            {
+
+
+                string fileName = @"C:\Temp\HelpPdfError.txt";
+
+
+                if (File.Exists(fileName))
+
+                {
+
+                    File.Delete(fileName);
+
+                }
+
+                // Create a new file     
+
+                using (FileStream fs = File.Create(fileName))
+
+                {
+                    // Add some text to file    
+
+                    Byte[] title = new UTF8Encoding(true).GetBytes("New Text File");
+
+                    fs.Write(title, 0, title.Length);
+                    byte[] text = new UTF8Encoding(true).GetBytes("ERROR------------------> " + e.StackTrace);
+
+                    fs.Write(text);
+                    return null;
+
+                }
+            }
         }
 
         public byte[] FillActivationPdfForm(string gId)
@@ -77,9 +148,9 @@ namespace EmployeeDeactivation.BusinessLayer
                     loadedListBox.SelectedValue = activationEmployeeData.Gender;
                 }
             }
-            var DateOfBirth = activationEmployeeData.DateOfBirth.Day + " / " + activationEmployeeData.DateOfBirth.Month + " / " + activationEmployeeData.DateOfBirth.Year;
+            var DateOfBirth = activationEmployeeData.DateOfBirth.Day + "/" + activationEmployeeData.DateOfBirth.Month + "/" + activationEmployeeData.DateOfBirth.Year;
             (form.Fields[9] as PdfLoadedTextBoxField).Text = DateOfBirth ?? "";
-            (form.Fields[9] as PdfLoadedTextBoxField).ReadOnly = true;
+            (form.Fields[9] as PdfLoadedTextBoxField).ReadOnly = false;
             (form.Fields[10] as PdfLoadedTextBoxField).Text = activationEmployeeData.LastName ?? "";
             (form.Fields[10] as PdfLoadedTextBoxField).ReadOnly = true;
             (form.Fields[11] as PdfLoadedTextBoxField).Text = activationEmployeeData.FirstName ?? "";
